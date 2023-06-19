1eeec1abab86690333eb5b2e0811d053ae749eea
awdem <wdemontmollin@gmail.com>
2023-06-19 17:05:45 +0100

    test: updates integration test

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe 'Integration' do
     xit 'returns a multi-line string of the bank statement' do
       account = BankAccount.new
 
-      account.deposit(1000, '10-01-2023')
-      account.deposit(2000, '13-01-2023')
-      account.withdraw(500, '14-01-2023')
+      account.deposit(1000, Time.new(2023, 1, 10))
+      account.deposit(2000, Time.new(2023, 1, 13))
+      account.withdraw(500, Time.new(2023, 1, 14))
 
       formatter = BankStatementFormatter.new
 
       formatted_statement = formatter.format(account.transactions)
 
-      expected_statement = <<~STATEMENT
+      expected_statement = <<~STATEMENT.chomp
         date || credit || debit || balance
         14/01/2023 || || 500.00 || 2500.00
         13/01/2023 || 2000.00 || || 3000.00
