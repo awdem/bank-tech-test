10d758576416ce46f67d9381a42c98203e9c302b
awdem <wdemontmollin@gmail.com>
2023-06-19 16:03:44 +0100

    test: corrects #withdraw tests so they correctly test for expected behavior

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -64,11 +64,11 @@ RSpec.describe 'BankAccount class' do
   end
 
   describe '#withdraw' do
-    context 'given a negative amount and a date' do
+    context 'given a positive amount and a date' do
       let(:date) { Time.new(2023, 7, 19) }
 
-      it 'adds a transaction hash to @transactions with the amount and date' do
-        account.withdraw(-500, date)
+      it 'adds a transaction hash to @transactions with the negative amount and date' do
+        account.withdraw(500, date)
 
         expect(account.transactions).to eq [
           { amount: -500.00, date: date }
@@ -76,14 +76,14 @@ RSpec.describe 'BankAccount class' do
       end
 
       it 'rounds amount to the hundreth decimal place' do
-        account.withdraw(-500.234, date)
+        account.withdraw(500.234, date)
 
         expect(account.transactions).to eq [
           { amount: -500.23, date: date }
         ]
       end
       it 'rounds amount up' do
-        account.withdraw(-500.235, date)
+        account.withdraw(500.235, date)
 
         expect(account.transactions).to eq [
           { amount: -500.24, date: date }
@@ -91,13 +91,13 @@ RSpec.describe 'BankAccount class' do
       end
     end
 
-    context 'given a negative amount with no date' do
+    context 'given a positive amount with no date' do
       it 'defaults to the current date' do
         today = Time.new(2023, 7, 19)
 
         allow(Time).to receive(:now).and_return(today)
 
-        account.withdraw(-500)
+        account.withdraw(500)
 
         expect(account.transactions).to eq [
           { amount: -500.00, date: today }
@@ -105,15 +105,15 @@ RSpec.describe 'BankAccount class' do
       end
     end
 
-    context 'given a positive amount' do
+    context 'given a negative amount' do
       it 'raises an error "amount must be below zero"' do
-        expect { account.withdraw(1) }.to raise_error 'amount must be below zero'
+        expect { account.withdraw(-1) }.to raise_error 'amount must be above zero'
       end
     end
 
     context 'given 0' do
       it 'raises an error "amount must be below zero"' do
-        expect { account.withdraw(0) }.to raise_error 'amount must be below zero'
+        expect { account.withdraw(0) }.to raise_error 'amount must be above zero'
       end
     end   
   end
