e34554ccc72fd36307556ed0bf9797c67b522f71
awdem <wdemontmollin@gmail.com>
2023-06-19 17:21:42 +0100

    feat: implements basic #format method that handles empty and single transaction accounts

--- lib/bank_statement_formatter.rb
+++ lib/bank_statement_formatter.rb
@@ -1,4 +1,15 @@
 # Produces a formatted bank statement from an array of transactions
 class BankStatementFormatter
-  
+  def format(transactions)
+    formatted_statement = 'date || credit || debit || balance'
+
+    transactions.each do |transaction|
+      transaction_date = transaction[:date].strftime('%d/%m/%Y')
+      transaction_amount = sprintf('%.2f', transaction[:amount])
+      transaction_line = "\n#{transaction_date} || #{transaction_amount} || || #{transaction_amount}"
+      formatted_statement << transaction_line
+    end
+
+    formatted_statement
+  end
 end
--- spec/bank_statement_formatter_spec.rb
+++ spec/bank_statement_formatter_spec.rb
@@ -6,6 +6,32 @@ RSpec.describe BankStatementFormatter do
     expect(formatter).to be_truthy
   end
 
-  
-end
+  context 'given an empty transaction array' do
+    it 'returns an empty statement' do
+      transactions = []
+      formatted_statement = formatter.format(transactions)
 
+      expected_statement = <<~STATEMENT.chomp
+        date || credit || debit || balance
+      STATEMENT
+
+      expect(formatted_statement).to eq(expected_statement)
+    end
+  end
+
+  context 'given a transaction array with one transacation' do
+    it 'returns a formatted statement' do
+      transactions = [
+        { amount: 500, date: Time.new(2023, 7, 19) }
+      ]
+      formatted_statement = formatter.format(transactions)
+
+      expected_statement = <<~STATEMENT.chomp
+        date || credit || debit || balance
+        19/07/2023 || 500.00 || || 500.00
+      STATEMENT
+
+      expect(formatted_statement).to eq(expected_statement)
+    end
+  end
+end
