2f785106e49140e28a4e5b68d2f35755fbda1b33
awdem <wdemontmollin@gmail.com>
2023-06-20 08:53:07 +0100

    feat: #format now formats multiple deposits

--- lib/bank_statement_formatter.rb
+++ lib/bank_statement_formatter.rb
@@ -1,15 +1,22 @@
 # Produces a formatted bank statement from an array of transactions
 class BankStatementFormatter
   def format(transactions)
-    formatted_statement = 'date || credit || debit || balance'
+    formatted_statement = "date || credit || debit || balance\n"
 
-    transactions.each do |transaction|
-      transaction_date = transaction[:date].strftime('%d/%m/%Y')
-      transaction_amount = sprintf('%.2f', transaction[:amount])
-      transaction_line = "\n#{transaction_date} || #{transaction_amount} || || #{transaction_amount}"
-      formatted_statement << transaction_line
+    transactions.reverse.each do |transaction|
+      transaction_string = create_transaction_string(transaction)
+      formatted_statement << transaction_string
     end
 
-    formatted_statement
+    formatted_statement.chomp
+  end
+
+  private
+
+  def create_transaction_string(transaction)
+    transaction_date = transaction[:date].strftime('%d/%m/%Y')
+    transaction_amount = sprintf('%.2f', transaction[:amount])
+    transaction_balance = sprintf('%.2f', transaction[:balance])
+    transaction_string = "#{transaction_date} || #{transaction_amount} || || #{transaction_balance}\n"
   end
 end
--- spec/bank_statement_formatter_spec.rb
+++ spec/bank_statement_formatter_spec.rb
@@ -19,15 +19,33 @@ RSpec.describe BankStatementFormatter do
     end
   end
 
-  context 'given a transaction array with one transacation' do
+  context 'given a transaction array with one deposit' do
     it 'returns a formatted statement' do
       transactions = [
-        { amount: 500, date: Time.new(2023, 7, 19) }
+        { amount: 500, date: Time.new(2023, 7, 19), balance: 500 }
       ]
       formatted_statement = formatter.format(transactions)
 
       expected_statement = <<~STATEMENT.chomp
         date || credit || debit || balance
+        19/07/2023 || 500.00 || || 500.00
+      STATEMENT
+
+      expect(formatted_statement).to eq(expected_statement)
+    end
+  end
+
+  context 'given a transaction array with multiple deposits' do
+    it 'returns a formatted statement in reverse chronological order' do
+      transactions = [
+        { amount: 500, date: Time.new(2023, 7, 19), balance: 500 },
+        { amount: 1000, date: Time.new(2023, 7, 20), balance: 1500 }
+      ]
+      formatted_statement = formatter.format(transactions)
+
+      expected_statement = <<~STATEMENT.chomp
+        date || credit || debit || balance
+        20/07/2023 || 1000.00 || || 1500.00
         19/07/2023 || 500.00 || || 500.00
       STATEMENT
 
